4a4c970d5b9043a3d77a67228ff79edff552bd37
drishal <drishalballaney@gmail.com>
2021-07-12 19:47:27 +0530

    fix st-flexipatch

---
+++ config/suckless/st-flexipatch/patch/x_include.h
@@ -0,0 +1,34 @@
+/* Patches */
+#if BOXDRAW_PATCH
+#include "boxdraw.h"
+#endif
+#if OPENCOPIED_PATCH
+#include "opencopied.h"
+#endif
+#if FONT2_PATCH
+#include "font2.h"
+#endif
+#if INVERT_PATCH
+#include "invert.h"
+#endif
+#if KEYBOARDSELECT_PATCH
+#include "keyboardselect_x.h"
+#endif
+#if NETWMICON_PATCH
+#include "netwmicon.h"
+#endif
+#if OPENURLONCLICK_PATCH
+#include "openurlonclick.h"
+#endif
+#if RIGHTCLICKTOPLUMB_PATCH
+#include "rightclicktoplumb_x.h"
+#endif
+#if ST_EMBEDDER_PATCH
+#include "st_embedder_x.h"
+#endif
+#if XRESOURCES_PATCH
+#include "xresources.h"
+#endif
+#if VIM_BROWSE_PATCH
+#include "normalMode.h"
+#endif
