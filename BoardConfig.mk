a6ddcd5840966b7d7f53c0b3eade6a74a5a3a5ef
FUKAUMI Naoki <naobsd@gmail.com>
2021-08-14 09:04:26 +0000

    alioth: Use qemu.sf.lcd_density instead of ro.sf.lcd_density 'qemu.sf.lcd_density' overrides xdpi and ydpi informations from framebuffer It's better to (ab)use 'qemu.sf.lcd_density' instead of patching frameworks/base
    Change-Id: I46c778bce351e0285995a7e8a606b83ccf93d548

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,9 +31,6 @@ AB_OTA_PARTITIONS += \
 # Board
 TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
 
-# Display
-TARGET_SCREEN_DENSITY := 420
-
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_alioth
 TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_alioth
@@ -45,6 +42,7 @@ TARGET_KERNEL_CONFIG := vendor/alioth_defconfig
 TARGET_OTA_ASSERT_DEVICE := alioth,aliothin
 
 # Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Inherit from the proprietary version
