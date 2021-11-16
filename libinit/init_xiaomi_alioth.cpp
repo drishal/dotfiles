ae9a90129efd45b328715ae0ec03827691e18a05
thunderkex <rizkywii@live.com>
2021-11-16 08:35:35 +0700

    alioth: Switch to Raven fingerprint

--- libinit/init_xiaomi_alioth.cpp
+++ libinit/init_xiaomi_alioth.cpp
@@ -9,7 +9,7 @@
 
 #include "vendor_init.h"
 
-#define FINGERPRINT "google/redfin/redfin:12/SP1A.211105.003/7757856:user/release-keys"
+#define FINGERPRINT "google/raven/raven:12/SD1A.210817.036/7805805:user/release-keys"
 
 static const variant_info_t aliothcn_info = {
     .hwc_value = "CN",
