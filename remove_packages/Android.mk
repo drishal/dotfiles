aab9ba65a3529e9506140e9204ac2c98ff987f90
drkphnx <dark.phnx12@gmail.com>
2022-01-11 21:39:13 +0530

    Remove SoundAmplifier and musicfx from debloater
    * We are not using Dolby so lets build prebuild MusicFx and soundamplifier

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := SoundAmplifierPrebuilt MusicFX FM2 MyVerizonServices OBDM_Permissions Showcase SprintDM SprintHM YouTube YouTubeMusicPrebuilt VZWAPNLib VzwOmaTrigger libqcomfm_jni obdm_stub qcom.fmradio Gallery2
+LOCAL_OVERRIDES_PACKAGES := FM2 MyVerizonServices OBDM_Permissions Showcase SprintDM SprintHM YouTube YouTubeMusicPrebuilt VZWAPNLib VzwOmaTrigger libqcomfm_jni obdm_stub qcom.fmradio Gallery2
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
