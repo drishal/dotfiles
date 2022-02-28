78673d6b6d8ef0b4fa47df2130ef338c747ded55
drishal <drishalballaney@gmail.com>
2022-02-28 10:07:27 +0530

    dmenu: some changes

--- suckless/dmenu/config.def.h
+++ suckless/dmenu/config.def.h
@@ -4,11 +4,11 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+"FiraCode Nerd Font:size=10", "JoyPixels:size=9:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
+	/*                 fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
 	[SchemeSel] = { "#eeeeee", "#005577" },
 	[SchemeOut] = { "#000000", "#00ffff" },
