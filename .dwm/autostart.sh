55c62355debbeb0eeb222d0a3fcc0b6593b662b8
drishal <drishalballaney@gmail.com>
2022-04-17 19:03:37 +0530

    update dwm autostart script

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,21 +1,34 @@
-/usr/lib/notification-daemon-1.0/notification-daemon&
+#/usr/lib/notification-daemon-1.0/notification-daemon&
+dunst&
 #spawnOnce "xsetroot -cursor_name left_
 #/usr/libexec/notification-daemon
+xset r rate  300 50
 lxpolkit&
 #polybar xmonad"
 #picom --experimental-backends&
-picom&
+picom --experimental-backends&
 #spawnOnce "picom"
 dwmblocks&
 #i3blocks&
 #i3blocks&
-#dwmstatus&
-nitrogen --restore&
+#
+feh --bg-scale ~/dotfiles/wallpapers/darkest_hour.jpg
 #spawnOnce "trayer --edge top --align right --widthtype request --SetDockType true --SetPartialStrut true --expand true  --transparent true  --tint 0x292d3e  --alpha 0 --height 20 --padding spawnOnce "trayer --edge top --align right --widthtype request --SetDockType true --SetPartialStrut true --expand true  --transparent true --alpha 0 --tint 0x282a36 --height 20 --padding 4"
 #        --spawnOnce "stalonetray"
 #        --spawnOnce "pasystray"
 nm-applet&
-mate-power-manager&
+# mate-power-manager&
 xfce4-clipman&
-redshift -O 4000 &
+xfce4-power-manager&
+emacs --daemon&
+
+blueman-applet&
+#redshift -O 5000 &
 volumeicon&
+# bash ~/.dwm/bar.sh&
+# scripts
+#!/bin/dash
+
+# ^c$var^ = fg color
+# ^b$var^ = bg color
+
