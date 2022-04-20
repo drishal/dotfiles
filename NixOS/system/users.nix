348ffebadab92e5f212992f8fe455d8871c176c9
drishal <drishalballaney@gmail.com>
2022-04-20 19:51:14 +0530

    add fprint support

--- NixOS/system/users.nix
+++ NixOS/system/users.nix
@@ -9,4 +9,10 @@
   security.sudo.extraConfig = ''
     Defaults   insults
   '';
+
+  # fprint
+  services.fprintd.enable = true;
+  security.pam.services.login.fprintAuth = true;
+  security.pam.services.sudo.fprintAuth = true;
+  security.pam.services.xscreensaver.fprintAuth = true;
 }
