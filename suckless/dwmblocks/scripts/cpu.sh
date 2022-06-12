51264807c771b8964442c7832b1e6a764a49cd8c
drishal <drishalballaney@gmail.com>
2022-06-12 12:03:24 +0530

    some tiny cosmetic improvements in scripts

--- suckless/dwmblocks/scripts/cpu.sh
+++ suckless/dwmblocks/scripts/cpu.sh
@@ -3,5 +3,5 @@
 # echo $(^c$magenta^ top -bn 1 | awk '/^%Cpu/ { print int($2 + $4 + $6)" %"}')
 cpu_val=$(grep -o "^[^ ]*" /proc/loadavg)
 
-printf " ^c$cyan^  CPU: $cpu_val %% "
+printf "^c$cyan^  CPU: $cpu_val %%"
 # printf "^c$green^ "
