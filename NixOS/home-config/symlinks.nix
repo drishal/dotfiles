e0b80b2c0075c197900ed8f2148666d2a4835adf
drishal <drishalballaney@gmail.com>
2022-06-21 17:58:56 +0530

    NixOS: change paths

--- NixOS/home-config/symlinks.nix
+++ NixOS/home-config/symlinks.nix
@@ -13,46 +13,48 @@
   # home.file."${pkgs.systemd}/lib/systemd/system-sleep/batdistrack".source = ../batdistrack;
 
   # deadd notifications
-  home.file."/home/drishal/.config/deadd".source = ../config/deadd;
+  home.file."/home/drishal/.config/deadd".source = ../../config/deadd;
 
   # rofi
-  # home.file."/home/drishal/.config/rofi/config.rasi".source = ../config/rofi/config.rasi;
+  # home.file."/home/drishal/.config/ro../../config.rasi".source = ../config/rofi/config.rasi;
 
-  # home.file."/home/drishal/.config/rofi/config".source = ../config/rofi/config;
+  # home.file."/home/drishal/.config/ro../../config".source = ../config/rofi/config;
 
   # setting Xresources
   # home.file."/home/drishal/.Xresources".source = ../.Xresources;
 
   # kitty
-  home.file."/home/drishal/.config/kitty".source = ../config/kitty;
+  home.file."/home/drishal/.config/kitty".source = ../../config/kitty;
 
   # picom config
-  home.file."/home/drishal/.config/picom/picom.conf".source = ../config/picom/picom.conf;
+  home.file."/home/drishal/.config/picom/picom.conf".source = ../../config/picom/picom.conf;
 
   # river
-  # home.file."/home/drishal/.config/river".source = ../config/river;
+  # home.file."/home/drishal/.config/river".source = ../../config/river;
 
   #waybar
-  home.file."/home/drishal/.config/waybar".source = ../config/waybar;
-  home.file."/home/drishal/.config/waybar/style.css".source = ../config/waybar/style.css;
+  home.file."/home/drishal/.config/waybar".source = ../../config/waybar;
+  home.file."/home/drishal/.config/waybar/style.css".source = ../../config/waybar/style.css;
 
   # alacritty
-  home.file."/home/drishal/.config/alacritty/alacritty.yml".source = ../config/alacritty/alacritty.yml;
+  home.file."/home/drishal/.config/alacritty/alacritty.yml".source = ../../config/alacritty/alacritty.yml;
 
   # dunst
-  home.file."/home/drishal/.config/dunst/dunstrc".source = ../config/dunst/dunstrc;
+  home.file."/home/drishal/.config/dunst/dunstrc".source = ../../config/dunst/dunstrc;
 
   # conky
-  home.file."/home/drishal/.config/conky/onedark.conkyrc".source = ../config/conky/onedark.conkyrc;
+  home.file."/home/drishal/.config/conky/onedark.conkyrc".source = ../../config/conky/onedark.conkyrc;
 
   #polybar
-  home.file."/home/drishal/.config/polybar/config.ini".source = ../config/polybar/config.ini;
+  home.file."/home/drishal/.config/polybar/config.ini".source = ../../config/polybar/config.ini;
 
   # leftwm
-  home.file."/home/drishal/.config/leftwm/config.toml".source = ../config/leftwm/config.toml;
-  home.file."/home/drishal/.config/leftwm/themes/current".source = ../config/leftwm/onedark;
+  home.file."/home/drishal/.config/leftwm/config.toml".source = ../../config/leftwm/config.toml;
+  home.file."/home/drishal/.config/leftwm/themes/current".source = ../../config/leftwm/onedark;
 
+  # awesomewm
+  home.file."/home/drishal/.config/awesome".source = ../../config/awesome;
   # qtile config
-  #home.file."/home/drishal/.config/qtile/config.py".source =../config/qtile/config.py;
-  # home.file."/home/drishal/.config/qtile/autostart.sh".source =../config/qtile/autostart.sh;
+  #home.file."/home/drishal/.config/qti../../config.py".source =../config/qtile/config.py;
+  # home.file."/home/drishal/.config/qtile/autostart.sh".source =../../config/qtile/autostart.sh;
 }
