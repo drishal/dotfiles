98fd598420972a85c017e2fe8765e643f7d4f1c9
drishal <drishalballaney@gmail.com>
2022-07-01 22:09:19 +0530

    home-config: update foot config

--- NixOS/home-config/terminals.nix
+++ NixOS/home-config/terminals.nix
@@ -1,14 +1,32 @@
 { config, inputs, pkgs, ... }:
 {
   programs = {
+    # foot terminal
     foot = {
       enable = true;
       server.enable = false;
       settings = {
         main = {
           term = "xterm-256color";
-          font = "FantasqueSansMono Nerd Font";
+          font = "FantasqueSansMono Nerd Font:size=8";
           dpi-aware = "yes";
+          pad = "15x4";
+        };
+        cursor = {
+          color = "282c34 51afef";
+        };
+        colors = {
+          # onedark
+          foreground = "bbc2cf";
+          background = "282c34";
+          regular0 = "282c34"; # black
+          regular1 = "ff6c6b"; # red
+          regular2 = "98be65"; # green
+          regular3 = "ecbe7b"; # yellow
+          regular4 = "51afef"; # blue
+          regular5 = "c678dd"; # magenta
+          regular6 = "46d9ff"; # cyan
+          regular7 = "bbc2cf"; # white
         };
       };
     };
