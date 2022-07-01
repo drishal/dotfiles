1f3bb4eef05dbba81638d1ede0c41372dda80367
drishal <drishalballaney@gmail.com>
2022-07-01 22:08:12 +0530

    NixOS: use brave

--- NixOS/home-config/browsers.nix
+++ NixOS/home-config/browsers.nix
@@ -3,7 +3,7 @@
   programs = {
     chromium = {
       enable = true;
-      # package = pkgs.ungoogled-chromium;
+      package = pkgs.brave;
       commandLineArgs = [
         "--ignore-gpu-blocklist"
         "--enable-gpu-rasterization"
