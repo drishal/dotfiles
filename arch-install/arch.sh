622a3df07fece542aafafb849b237627275dbf46
drishal <drishalballaney@gmail.com>
2022-10-07 22:07:23 +0530

    update dotfiles

--- arch-install/arch.sh
+++ arch-install/arch.sh
@@ -1,13 +1,21 @@
-pacman -S base base-devel efibootmgr grub linux linux-headers linux-firmware vim nano networkmanager iwd --needed
+pacman -S base base-devel efibootmgr grub linux linux-headers linux-firmware vim nano networkmanager iwd amd-ucode --needed
 echo "setting hostnam"
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
-echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
+echo "127.0.1.1 arch.localdomain archlinux" >> /etc/hosts
+echo "archlinux" >> /etc/hostname
 
+# echo "Fstab"
+# genfstab -U /mnt >> /mnt/etc/fstab
 
 echo "setting locale"
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
-echo "LANG=en_US.UTF-8" >> /etc/locale.gen
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+
+echo "Setting timezone"
+ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
+hwclock --systohc
 
 echo "installing grub"
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
@@ -16,4 +24,6 @@ grub-mkconfig -o /boot/grub/grub.cfg
 echo "enabling system services"
 systemctl enable NetworkManager
 systemctl enable iwd
+
+"Setting root password"
 passwd 
