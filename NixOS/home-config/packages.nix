6c97546c4dca56a084903034fc41cad86ed4fc0d
drishal <drishalballaney@gmail.com>
2023-12-16 19:21:33 +0530

    nix: enable waybar

--- NixOS/home-config/packages.nix
+++ NixOS/home-config/packages.nix
@@ -60,15 +60,16 @@
     };
   };
 
-   # programs.waybar = {
-   #   enable = false;
-   #   package = pkgs.waybar.overrideAttrs (oldAttrs: {
-   #     mesonFlags = oldAttrs.mesonFlags ++ ["-Dexperimental=true"];
-   #     postPatch = ''
-   #       substituteInPlace src/modules/wlr/workspace_manager.cpp --replace "zext_workspace_handle_v1_activate(workspace_handle_);" "const std::string command = \"${config.wayland.windowManager.hyprland.package}/bin/hyprctl dispatch workspace \" + name_; system(command.c_str());"
-   #     '';
-   #   });
-   # };
+    programs.waybar = {
+      enable = true;
+      # package = pkgs.waybar-hyprland;
+      #package = pkgs.waybar.overrideAttrs (oldAttrs: {
+      #  mesonFlags = oldAttrs.mesonFlags ++ ["-Dexperimental=true"];
+      #  postPatch = ''
+      #    substituteInPlace src/modules/wlr/workspace_manager.cpp --replace "zext_workspace_handle_v1_activate(workspace_handle_);" "const std::string command = \"${config.wayland.windowManager.hyprland.package}/bin/hyprctl dispatch workspace \" + name_; system(command.c_str());"
+      #  '';
+      # });
+    };
 
   # gtk = {
   #   enable = true;
