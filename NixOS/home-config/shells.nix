7b0748fb6ad55fdde02bdf5f81c48fb3bb6c3904
drishal <drishalballaney@gmail.com>
2023-12-26 18:36:02 +0530

    fish: move fish to nix config

--- NixOS/home-config/shells.nix
+++ NixOS/home-config/shells.nix
@@ -3,42 +3,64 @@
   # lib.fakeSha256
 
   programs.fish = {
-    enable=false;
+    enable=true;
     interactiveShellInit = ''
-    # color schemes
+    # paths
+    fish_add_path ~/.local/bin
+    fish_add_path ~/.local/bin/platform-tools
+    fish_add_path ~/.node_modules/bin
+    fish_add_path ~/.nimble/bin
+    fish_add_path ~/.cargo/bin
+
+    #colors
     set fish_color_normal '#98be65'
     set fish_color_autosuggestion '#6272a4'
     set fish_color_command '#98be65'
     set fish_color_error '#ff6c6b'
     set fish_color_param '#98be65'
 
-    # set greeting to blank 
+    # greeting
     set fish_greeting
 
-    # starship
+    #starship
     starship init fish | source
+    set -x STARSHIP_CONFIG ~/dotfiles/config/starship.toml
 
-    # bass
-    # bass source /etc/profile
-    bass source ~/dotfiles/.profile
+    #newline
+    function postexec_test --on-event fish_postexec
+        echo
+    end
 
-    # add to path
-    fish_add_path ~/.local/bin
+    #manpager
+    set -x MANROFFOPT "-c" 
+    set -x MANPAGER "sh -c 'col -bx | bat -plman'"
+
+    #direnv
+    direnv hook fish | source
+    set -x DIRENV_LOG_FORMAT ""
+
+    #lutris skip 
+    set -x LUTRIS_SKIP_INIT 1
+
+    #aliases
+    source ~/dotfiles/aliases.sh
+
     '';
     plugins = [
       # Enable a plugin (here grc for colorized command output) from nixpkgs
       # { name = "grc"; src = pkgs.fishPlugins.grc.src; }
       # Manually packaging and enable a plugin
-      {
-        name = "bass";
-        src = pkgs.fetchFromGitHub {
-          owner = "edc";
-          repo = "Bass";
-          rev = "2fd3d2157d5271ca3575b13daec975ca4c10577a";
-          # sha256 = "0dbnir6jbwjpjalz14snzd3cgdysgcs3raznsijd6savad3qhijc";
-          sha256 = "sha256-fl4/Pgtkojk5AE52wpGDnuLajQxHoVqyphE90IIPYFU=";
-        };
-      }
+      # {
+      #   name = "bass";
+      #   src = pkgs.fetchFromGitHub {
+      #     owner = "edc";
+      #     repo = "Bass";
+      #     rev = "79b62958ecf4e87334f24d6743e5766475bcf4d0";
+      #     sha256 = "sha256-3d/qL+hovNA4VMWZ0n1L+dSM1lcz7P5CQJyy+/8exTc=";
+      #   };
+      # }
+     
+     { name = "fzf-fish"; src = pkgs.fishPlugins.fzf-fish.src; }
     ];
   };
 
