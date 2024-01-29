0de183f65451821439b0a474eafbe96213865b6a
drishal <drishalballaney@gmail.com>
2024-01-29 10:56:10 +0530

    home manager: add tmux config

--- NixOS/home-config/tmux.nix
+++ NixOS/home-config/tmux.nix
@@ -1,9 +1,15 @@
 { config, inputs, pkgs, ... }:
-
-programs.tmux = {
-  enable = true;
-  mouse = true;
-  plugins = with pkgs; [
-    tmuxPlugins.sensible
-  ];
-};
+{
+  programs.tmux = {
+    enable = true;
+    mouse = true;
+    terminal = "xterm";
+    plugins = with pkgs; [
+      tmuxPlugins.sensible
+      tmuxPlugins.vim-tmux-navigator
+      tmuxPlugins.onedark-theme
+    ];
+    extraConfig = ''
+    '';
+  };
+}
