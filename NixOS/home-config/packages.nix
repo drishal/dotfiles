c2f9949fddc4b29aab20148f769f30e9cb85026c
drishal <drishalballaney@gmail.com>
2024-01-29 10:55:53 +0530

    home manager: move waybar config to waybar.nix

--- NixOS/home-config/packages.nix
+++ NixOS/home-config/packages.nix
@@ -65,16 +65,6 @@
     };
   };
 
-  programs.waybar = {
-    enable = true;
-    # package = pkgs.waybar-hyprland;
-    #package = pkgs.waybar.overrideAttrs (oldAttrs: {
-    #  mesonFlags = oldAttrs.mesonFlags ++ ["-Dexperimental=true"];
-    #  postPatch = ''
-    #    substituteInPlace src/modules/wlr/workspace_manager.cpp --replace "zext_workspace_handle_v1_activate(workspace_handle_);" "const std::string command = \"${config.wayland.windowManager.hyprland.package}/bin/hyprctl dispatch workspace \" + name_; system(command.c_str());"
-    #  '';
-    # });
-  };
 
   gtk = {
     enable = true;
