88b5ee29e6fbe74174eefc80d78d1b78e7dc725b
drishal <drishalballaney@gmail.com>
2024-02-27 14:47:29 +0530

    home manager: switch back to nixvim

--- NixOS/home-config/editors.nix
+++ NixOS/home-config/editors.nix
@@ -16,52 +16,52 @@
     };
 
     nixvim = {
-      enable = false;
+      enable = true;
       enableMan = true;
       colorschemes.onedark.enable = true;
       extraConfigLua = ''
-        -- Set highlight on search
-        vim.o.hlsearch = false
+      -- Set highlight on search
+      vim.o.hlsearch = false
 
-        -- Make line numbers default
-        vim.wo.number = true
+      -- Make line numbers default
+      vim.wo.number = true
 
-        -- Enable mouse mode
-        vim.o.mouse = 'a'
+      -- Enable mouse mode
+      vim.o.mouse = 'a'
 
-        -- Sync clipboard between OS and Neovim.
-        --  Remove this option if you want your OS clipboard to remain independent.
-        --  See `:help 'clipboard'`
-        vim.o.clipboard = 'unnamedplus'
+      -- Sync clipboard between OS and Neovim.
+      --  Remove this option if you want your OS clipboard to remain independent.
+      --  See `:help 'clipboard'`
+      vim.o.clipboard = 'unnamedplus'
 
-        -- Enable break indent
-        vim.o.breakindent = true
+      -- Enable break indent
+      vim.o.breakindent = true
 
-        -- Save undo history
-        vim.o.undofile = true
-        -- Case-insensitive searching UNLESS \C or capital in search
-        vim.o.ignorecase = true
-        vim.o.smartcase = true
+      -- Save undo history
+      vim.o.undofile = true
+      -- Case-insensitive searching UNLESS \C or capital in search
+      vim.o.ignorecase = true
+      vim.o.smartcase = true
 
-        -- Keep signcolumn on by default
-        vim.wo.signcolumn = 'yes'
+      -- Keep signcolumn on by default
+      vim.wo.signcolumn = 'yes'
 
-        -- Decrease update time
-        vim.o.updatetime = 250
-        vim.o.timeoutlen = 300
+      -- Decrease update time
+      vim.o.updatetime = 250
+      vim.o.timeoutlen = 300
 
-        -- Set completeopt to have a better completion experience
-        vim.o.completeopt = 'menuone,noselect'
+      -- Set completeopt to have a better completion experience
+      vim.o.completeopt = 'menuone,noselect'
 
-        -- NOTE: You should make sure your terminal supports this
-        vim.o.termguicolors = true
+      -- NOTE: You should make sure your terminal supports this
+      vim.o.termguicolors = true
       '';
-        extraPackages = with pkgs;[
-                         vimPlugins.nvim-web-devicons
-                       ];
-        extraPlugins = with pkgs.vimPlugins;[
-          orgmode
-        ];
+      extraPackages = with pkgs;[
+        vimPlugins.nvim-web-devicons
+      ];
+      extraPlugins = with pkgs.vimPlugins;[
+        orgmode
+      ];
       clipboard.register="unnamedplus";
       #lsp config
       plugins = {
@@ -124,20 +124,20 @@
             "<CR>" = "cmp.mapping.confirm({ select = true })";
             "<Tab>" = {
               action = ''
-            function(fallback)
+              function(fallback)
               if cmp.visible() then
-                cmp.select_next_item()
+              cmp.select_next_item()
               elseif luasnip.expandable() then
-                luasnip.expand()
+              luasnip.expand()
               elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
+              luasnip.expand_or_jump()
               elseif check_backspace() then
-                fallback()
+              fallback()
               else
-                fallback()
+              fallback()
               end
-            end
-          '';
+              end
+              '';
               modes = [ "i" "s" ];
             };
           };
@@ -188,6 +188,9 @@
             inc_rename = false; # enables an input dialog for inc-rename.nvim
             lsp_doc_border = false; # add a border to hover docs and signature help
           };
+        };
+        none-ls = {
+          enable = true;
         };
       };
     };
