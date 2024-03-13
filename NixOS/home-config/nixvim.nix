14fc22b79b86a28941b62d35e14ed336ffea4d6a
drishal <drishalballaney@gmail.com>
2024-03-13 10:04:04 +0530

    nixvim: remove lua code

--- NixOS/home-config/nixvim.nix
+++ NixOS/home-config/nixvim.nix
@@ -5,38 +5,20 @@
     enable = true;
     enableMan = true;
     colorschemes.onedark.enable = true;
-    extraConfigLua = ''
-      -- Set highlight on search
-      vim.o.hlsearch = false
-
-      -- Make line numbers default
-      vim.wo.number = true
-
-      -- Enable mouse mode
-      vim.o.mouse = 'a'
-
-      -- Enable break indent
-      vim.o.breakindent = true
-
-      -- Save undo history
-      vim.o.undofile = true
-      -- Case-insensitive searching UNLESS \C or capital in search
-      vim.o.ignorecase = true
-      vim.o.smartcase = true
-
-      -- Keep signcolumn on by default
-      vim.wo.signcolumn = 'yes'
-
-      -- Decrease update time
-      vim.o.updatetime = 250
-      vim.o.timeoutlen = 300
-
-      -- Set completeopt to have a better completion experience
-      vim.o.completeopt = 'menuone,noselect'
-
-      -- NOTE: You should make sure your terminal supports this
-      vim.o.termguicolors = true
-    '';
+    options = {
+      hlsearch = false;
+      number = true;
+      mouse = "a";
+      breakindent = true;
+      undofile = true;
+      ignorecase = true;
+      smartcase = true;
+      signcolumn = "yes";
+      updatetime = 250;
+      timeoutlen = 300;
+      completeopt = "menuone,noselect";
+      termguicolors = true;
+    };
     extraPackages = with pkgs;[
       vimPlugins.nvim-web-devicons
     ];
