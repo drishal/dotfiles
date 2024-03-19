67d14a9500fd9b5456ba7df8a95b2bc58a2f4b54
drishal <drishalballaney@gmail.com>
2024-03-19 23:28:22 +0530

    home configuration: doom-one.nix: cleanup

--- NixOS/home-config/colors/doomone.nix
+++ NixOS/home-config/colors/doomone.nix
@@ -11,39 +11,14 @@
       base05 = "BBC2CF"; #white
       base06 = "BBC2CF";
       base07 = "BBC2CF";
-      base08 = "FF6C6B"; #brightred
-      base09 = "DA8548"; #
+      base08 = "FF6C6B"; #red
+      base09 = "DA8548"; #orange
       base0A = "ECBE7B"; #yellow
       base0B = "98BE65"; #green
       base0C = "46D9FF"; #cyan 
       base0D = "51AFEF"; #blue
       base0E = "C678DD"; #magenta
-      base0F = "A9A1E1";
+      base0F = "A9A1E1"; #violet
     };
   };
-
-  # colorScheme = {
-  # name = "palenight"; 
-  #   colors = {
-  #     base00 = "292D3E";
-  #     base01 = "676E95";
-  #     base02 = "3F444A";
-  #     base03 = "5B6268"; #brightblack
-  #     base04 = "BBC2CF";
-  #     base05 = "BBC2CF"; #white
-  #     base06 = "BBC2CF";
-  #     base07 = "BBC2CF";
-  #     base08 = "FF5370"; #brightred
-  #     base09 = "DA8548";
-  #     base0A = "F78C6C"; #orange
-  #     base0B = "C3E88D"; #green
-  #     base0C = "89DDFF"; #cyan
-  #     base0D = "82AAFF"; #blue
-  #     base0E = "C792EA"; #magenta
-  #     base0F = "BB80B3";
-  #   };
-  # };
-
-  # ./modules/myModule.nix
-
 }
