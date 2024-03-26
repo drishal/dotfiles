189ace5697acb1812d67bf73e8d1b5b87f735418
drishal <drishalballaney@gmail.com>
2024-03-26 10:11:03 +0530

    nix colors: palenight: redo

--- NixOS/home-config/colors/doompalenight.nix
+++ NixOS/home-config/colors/doompalenight.nix
@@ -4,21 +4,21 @@
     name = "palenight";
     palette = {
       base00 = "292D3E";
-      base01 = "676E95";
-      base02 = "3F444A";
-      base03 = "5B6268"; #brightblack
-      base04 = "BBC2CF";
-      base05 = "BBC2CF"; #white
-      base06 = "BBC2CF";
-      base07 = "BBC2CF";
-      base08 = "FF5370"; #brightred
-      base09 = "DA8548";
-      base0A = "F78C6C"; #orange
+      base01 = "444267";
+      base02 = "32374D";
+      base03 = "676E95"; #brightblack
+      base04 = "8796B0";
+      base05 = "959DCB"; #white
+      base06 = "959DCB";
+      base07 = "FFFFFF";
+      base08 = "F07178"; #red
+      base09 = "F78C6C"; #orange
+      base0A = "FFCB6B"; #yellow
       base0B = "C3E88D"; #green
       base0C = "89DDFF"; #cyan
       base0D = "82AAFF"; #blue
-      base0E = "C792EA"; #magenta
-      base0F = "BB80B3";
+      base0E = "C792EA"; #magent
+      base0F = "BB80B3"; #violet
     };
   };
 }
