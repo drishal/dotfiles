62e159d07c9447e2914c18df5b8939a378f85214
drishal <drishalballaney@gmail.com>
2024-04-17 17:17:14 +0530

    home-config: rofi: cleanup

--- NixOS/home-config/rofi.nix
+++ NixOS/home-config/rofi.nix
@@ -8,12 +8,12 @@
   programs = {
     rofi = {
       enable = true;
-      theme = "${pkgs.rofi}/share/rofi/themes/Arc-Dark.rasi";
+      theme = "${pkgs.rofi-wayland}/share/rofi/themes/Arc-Dark.rasi";
       # font="FiraCode Nerd Font 14";
       font = "FantasqueSansMono Nerd Font 14";
-      plugins = [
-        pkgs.rofi-emoji
-        pkgs.glibcLocales
+      plugins = with pkgs; [
+        rofi-emoji
+        glibcLocales
       ];
     };
   };
