49a66cbc3d034dd8a3e9e8f69229931cd45d1877
drishal <drishalballaney@gmail.com>
2024-04-24 15:14:07 +0530

    hardware-configuration: iwlwifi changes

--- NixOS/system-config/nixos/hardware-configuration.nix
+++ NixOS/system-config/nixos/hardware-configuration.nix
@@ -54,4 +54,7 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  boot.extraModprobeConfig = ''
+  options iwlwifi 11n_disable=8
+ '';
 }
