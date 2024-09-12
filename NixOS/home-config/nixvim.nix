79ad5cbc41154e309b0aa0f048793871837ccbdc
drishal <drishalballaney@gmail.com>
2024-09-12 18:26:51 +0530

    nixvim: disable rustaceanvim.

--- NixOS/home-config/nixvim.nix
+++ NixOS/home-config/nixvim.nix
@@ -121,7 +121,6 @@
           };
         };
         lspkind.enable = true;
-        rustaceanvim.enable = true;
         # lazy = {
         #   enable = true;
         #   plugins = with pkgs.vimPlugins; [
