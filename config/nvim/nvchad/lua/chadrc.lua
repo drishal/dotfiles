16fbb562f0311555603f87533ab87d6e66bf4559
drishal <drishalballaney@gmail.com>
2024-11-08 15:54:54 +0530

    update

---
+++ config/nvim/nvchad/lua/chadrc.lua
@@ -0,0 +1,21 @@
+-- This file needs to have same structure as nvconfig.lua 
+-- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
+-- Please read that file to know all available options :( 
+
+---@type ChadrcConfig
+local M = {}
+local bruh = {
+  everforest = {Normal = {bg="#272E33"}},
+}
+
+M.base46 = {
+	theme = "everforest",
+
+	hl_override = {
+		Comment = { italic = true },
+		["@comment"] = { italic = true },
+	},
+}
+M.base46.hl_override = bruh[M.base46.theme]
+
+return M
