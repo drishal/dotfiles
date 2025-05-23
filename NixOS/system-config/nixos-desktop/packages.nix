da506da9633704cad8ec56f7fe84aaa68298e026
Drishal <drishalballaney@gmail.com>
2025-05-23 10:32:58 +0530

    update

--- NixOS/system-config/nixos-desktop/packages.nix
+++ NixOS/system-config/nixos-desktop/packages.nix
@@ -1,6 +1,7 @@
 { pkgs, inputs, ... }:
 
 {
+  networking.enableIPv6  = false;
   environment.systemPackages = with pkgs; [
     inputs.nix-gaming.packages.${pkgs.system}.wine-tkg
     inputs.nix-gaming.packages.${pkgs.system}.winetricks-git
