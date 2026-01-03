e0ab3441a3059c406a3fae1a5e98b17b31500d09
drishal <drishalballaney@gmail.com>
2026-01-03 12:04:02 +0530

    update

--- NixOS/system-config/packages.nix
+++ NixOS/system-config/packages.nix
@@ -741,6 +741,7 @@
   services.syncthing = {
     enable = true;
     openDefaultPorts = true; # Open ports in the firewall for Syncthing. (NOTE: this will not open syncthing gui port)
+    guiAddress = "0.0.0.0:8384";
   };
 
 
