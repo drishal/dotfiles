19d5b9f4371bfb5cd15427910e29f61396fa227e
drishal <drishalballaney@gmail.com>
2026-05-12 17:03:07 +0530

    refactor(editors): switch Emacs to emacsWithPackagesFromUsePackage, fix formatting
    Uncomment the emacswithPackagesFromUsePackage block with treesit grammars
    and eglot-booster now that elpaca is disabled in config.org. Remove the
    redundant programs.emacs block. Fix leading indentation, nixfmt-compliant
    list formatting, and closing brace alignment.

--- NixOS/home-config/editors.nix
+++ NixOS/home-config/editors.nix
@@ -1,4 +1,4 @@
-    {
+{
   config,
   inputs,
   pkgs,
@@ -9,35 +9,35 @@
 # Editors
 {
   imports = [
-     ./nixvim.nix
+    ./nixvim.nix
   ];
-  # home.packages = with pkgs; [
-  #   (pkgs.emacsWithPackagesFromUsePackage {
-  #     config = ../../emacs/config.org;
-  #     package = pkgs.emacs-unstable-pgtk;
-  #     alwaysEnsure = true;
-  #     alwaysTangle = true;
-  #     extraEmacsPackages =
-  #       epkgs: with epkgs; [
-  #         use-package
-  #         treesit-grammars.with-all-grammars
-  #         vterm
-  #       ];
-  #     override = final: prev: {
-  #       rustic = prev.rustic.overrideAttrs { ignoreCompilationError = true; };
-  #       eglot-booster = final.melpaBuild {
-  #         pname = "eglot-booster";
-  #         version = "0.1.0.0.20240616";
-  #         src = pkgs.fetchFromGitHub {
-  #           owner = "jdtsmith";
-  #           repo = "eglot-booster";
-  #           rev = "cab7803c4f0adc7fff9da6680f90110674bb7a22";
-  #           hash = "sha256-xUBQrQpw+JZxcqT1fy/8C2tjKwa7sLFHXamBm45Fa4Y=";
-  #         };
-  #       };
-  #     };
-  #   })
-  # ];
+  home.packages = with pkgs; [
+    (pkgs.emacsWithPackagesFromUsePackage {
+      config = ../../emacs/config.org;
+      package = pkgs.emacs-unstable-pgtk;
+      alwaysEnsure = true;
+      alwaysTangle = true;
+      extraEmacsPackages =
+        epkgs: with epkgs; [
+          use-package
+          treesit-grammars.with-all-grammars
+          vterm
+        ];
+      override = final: prev: {
+        rustic = prev.rustic.overrideAttrs { ignoreCompilationError = true; };
+        eglot-booster = final.melpaBuild {
+          pname = "eglot-booster";
+          version = "0.1.0.0.20240616";
+          src = pkgs.fetchFromGitHub {
+            owner = "jdtsmith";
+            repo = "eglot-booster";
+            rev = "cab7803c4f0adc7fff9da6680f90110674bb7a22";
+            hash = "sha256-xUBQrQpw+JZxcqT1fy/8C2tjKwa7sLFHXamBm45Fa4Y=";
+          };
+        };
+      };
+    })
+  ];
   programs = {
     micro = {
       enable = false;
@@ -46,21 +46,24 @@
       enable = true;
       extraConfig = ''
         ${builtins.readFile ../../config/helix/config.toml}
-        '';
+      '';
     };
-    emacs = {
-      enable = true;
-      package = pkgs.emacs-unstable-pgtk;
-      extraPackages =
-        epkgs: with epkgs; [
-          vterm
-          # treesit-grammars.with-all-grammars
-        ];
-    };
+    # emacs = {
+    #   enable = true;
+    #   package = pkgs.emacs-unstable-pgtk;
+    #   extraPackages =
+    #     epkgs: with epkgs; [
+    #       vterm
+    #       # treesit-grammars.with-all-grammars
+    #     ];
+    # };
 
     zed-editor = {
       enable = true;
-      extensions = ["nix" "rust"];
+      extensions = [
+        "nix"
+        "rust"
+      ];
       userSettings = {
         # ui_font_size = 16;
         # buffer_font_size = 16.0;
@@ -72,7 +75,7 @@
           light = lib.mkForce "One Light";
         };
       };
-      };
+    };
     # nvchad = {
     #   enable = true;
     #   extraPackages = with pkgs; [
