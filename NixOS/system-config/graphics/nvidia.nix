58897fcefe4b077c2a599def827f0036b6740c73
drishal <drishalballaney@gmail.com>
2026-05-12 17:03:00 +0530

    fix(nvidia): add GBM_BACKEND and NVD_BACKEND session variables
    These are recommended by the Hyprland NVIDIA wiki for proper DRM
    buffer management and direct rendering support on Wayland.

--- NixOS/system-config/graphics/nvidia.nix
+++ NixOS/system-config/graphics/nvidia.nix
@@ -82,6 +82,8 @@
   environment.sessionVariables = {
     LIBVA_DRIVER_NAME = "nvidia";
     __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    NVD_BACKEND = "direct" ;
+    GBM_BACKEND = "nvidia-drm";
     # VK_ICD_FILENAMES = "/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.x86_64.json";
     # VK_DRIVER_FILES = "/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.json";
 
