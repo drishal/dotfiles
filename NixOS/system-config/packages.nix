4d419e98f6aaf373443783ea9d3c0634b006c673
drishal <drishalballaney@gmail.com>
2026-05-15 22:27:39 +0530

    fix(packages): pin bun to v1.3.14 via overrideAttrs

--- NixOS/system-config/packages.nix
+++ NixOS/system-config/packages.nix
@@ -157,7 +157,14 @@
     bison
     # brave
     btop
-    bun
+    # pkgs-master.bun
+    (bun.overrideAttrs (old: rec {                                                                                                                                                         
+     version = "1.3.14";                                                                                                                                                                  
+     src = pkgs.fetchurl {                                                                                                                                                                
+       url = "https://github.com/oven-sh/bun/releases/download/bun-v${version}/bun-linux-x64.zip";                                                                                        
+       hash = "sha256-lR7iruhV8IWVruxiJSJqKY0/6oOj3NZGXAnLzN9+hI8=";                                                                                                                      
+     };                                                                                                                                                                                   
+   }))
     brightnessctl
     bridge-utils
     bubblewrap
